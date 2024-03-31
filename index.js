fc437a11828f1b3819e5d1366aac6ca6d1dfb0f6
Zafar Hussain <hszafar3@gmail.com>
2024-03-31 03:20:29 -0700

    first commit1

---
+++ index.js
@@ -0,0 +1,15 @@
+// operators
+// arithmatic
+// -addition
+var x = 2;
+var y = 5;
+var add = 2 + 5; //7
+var add1 = "x" + "y"; //xy
+var add2 = "xy" + x + y; //xy25
+var add3 = "xy" + add; //xy7
+var add4 = x + y + "xy"; //7xy
+console.log("add: ", add);
+console.log("add1:", add1);
+console.log("add2:", add2);
+console.log("add3:", add3);
+console.log("add4:", add4);
---
+++ index.ts
@@ -0,0 +1,27 @@
+// operators
+
+// arithmatic
+// -addition
+let x = 2;
+let y = 5;
+
+let add = 2 + 5; //7
+let add1 = "x" + "y"; //xy
+let add2 = "xy" + x + y; //xy25
+let add3 = "xy" + add; //xy7
+let add4 = x + y + "xy"; //7xy
+// let add6:number=x+"xy";
+// let add7:number="xy"+x;
+let add6: string = x + "xy";
+let add7: string = "xy" + x;
+console.log("add: ", add);
+console.log("add1:", add1);
+console.log("add2:", add2);
+console.log("add3:", add3);
+console.log("add4:", add4);
+
+// -subtraction
+let sub = x - y; //-3
+//letsub1="x"-"y";//error
+
+//multiplcation
